abdc4f04fc8018884b56cda0ad7025fc881216e9
aminata <caminatamagath@gamil.com>
2024-01-23 11:19:34 +0100

    notebook work again

---
+++ projet_data_analyst.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+Customers = pd.read_csv('customers_dataset.csv')
+
+Geolocation = pd.read_csv('geolocation_dataset.csv')
+
+Order_items = pd.read_csv('items_dataset.csv')
+
+Order_payments= pd.read_csv('payments_dataset.csv')
+
+Order_review_clean = pd.read_csv('review_dataset_clean.csv')
+
+Orders = pd.read_csv('orders_dataset.csv')
+
+Product_category = pd.read_csv('product_category_name_translation.csv')
+
+Product = pd.read_csv('products_dataset.csv')
+
+Sellers = pd.read_csv('sellers_dataset.csv')
+
+State_name = pd.read_csv('state_name.csv')
+
